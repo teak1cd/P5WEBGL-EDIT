45c31c587ff1ebb505fdff01e95e05f4e6e909f8
teak1cd <isenne@sterneschool.org>
2016-11-17 08:21:03 -0800

    Update editor.js

--- editor.js
+++ editor.js
@@ -14,20 +14,22 @@ function addObject(){
   console.log("addobj");
   var tempOBJ = {};
   tempOBJ.type = document.getElementsByTagName("input")[0].value;
+  if(tempOBJ.type==="")return;
   tempOBJ.inputs = [];
-  tempOBJ.inputs[0] = document.getElementsByTagName("input")[1].value;
-  tempOBJ.inputs[1] = document.getElementsByTagName("input")[2].value;
-  tempOBJ.inputs[2] = document.getElementsByTagName("input")[3].value;
-  tempOBJ.inputs[3] = document.getElementsByTagName("input")[4].value;
-  tempOBJ.inputs[4] = document.getElementsByTagName("input")[5].value;
-  tempOBJ.inputs[5] = document.getElementsByTagName("input")[6].value;
+  tempOBJ.inputs[0] = parseFloat(document.getElementsByTagName("input")[1].value);
+  tempOBJ.inputs[1] = parseFloat(document.getElementsByTagName("input")[2].value);
+  tempOBJ.inputs[2] = parseFloat(document.getElementsByTagName("input")[3].value);
+  tempOBJ.inputs[3] = parseFloat(document.getElementsByTagName("input")[4].value);
+  tempOBJ.inputs[4] = parseFloat(document.getElementsByTagName("input")[5].value);
+  tempOBJ.inputs[5] = parseFloat(document.getElementsByTagName("input")[6].value);
   tempOBJ.inputs[6] = 0;
   if(tempOBJ.type==="Line")tempOBJ.inputs[6] = document.getElementsByTagName("input")[7].value;
   levelJSON.objects.push(tempOBJ);
-  var frame = document.getElementsByTagName("iframe")[0].contentWindow;
+  var frame = document.getElementById("iframe").contentWindow;
   frame.engine.objects = frame.level.getData(levelJSON)[0];
   frame.engine.textures = frame.level.getData(levelJSON)[1];
   rebuildLevelData();
+  document.getElementById("objects").innerHTML+='<span class="object" style="width:100%;height:10px;background-color:rgb(200,200,200);"><span class="object_name">%name%</span><button class="removeObj" onClick="removeOBJ(%n%);">X</button><button class="editObj" onClick="editOBJ(%n%);">Edit</button></span>'.replace(/%name%/gm,tempOBJ.type).replace(/%n%/gm,levelJSON.objects.length-1);
 }
 function addTexture(){
   var ts = document.getElementById("textureSelector");
